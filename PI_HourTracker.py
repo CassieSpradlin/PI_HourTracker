b9ea4e2e635e10b94732fcec5da878c34d087497
Cassie <cassie@Daniels-MacBook-Pro.local>
2021-03-02 07:40:33 -0500

    adding features for returning records

--- PI_HourTracker.py
+++ PI_HourTracker.py
@@ -2,6 +2,18 @@
 #data = pd.read_csv('/Users/cassie/Documents/Project_Folder/PI_HourTracker/inventory_hours.csv')
 #data.head()
 
+clients_byWHS = {
+    "Dog Toys Ltd" : "January",
+    "Guitar World": "February",
+    "Candles and Candles": "April",
+    "Purses and More": "July"   
+}
+
+pi_due = input("Enter client for PI due date: ")
+
+print(clients_byWHS[pi_due])
+
+
 import sqlite3 as db
 
 def init():
@@ -24,9 +36,15 @@ record_list = []
 def show_help():
     print("Add data record for Physical Inventory")
     print("""
-Enter 'New Record' to add another record.
+Enter 'NR' to add another record.
 Enter 'DONE' to stop adding records.
 Enter 'HELP' for this help.
+
+Tasks can include:
+Prep Work
+Physical Counts
+Auditing
+Clean Up
 """)
 
 show_help()
@@ -71,14 +89,12 @@ def new_entry():
 
 while True:
     new_record = input("> ")
-    if new_record == 'New Record':
+    if new_record == 'NR':
         new_entry()
     elif new_record == 'DONE':
         break
     elif new_record == 'HELP':
         break
-
-
 
 
 
@@ -98,3 +114,25 @@ def view(task=None):
     return results
 
 print (view())
+
+def getClientInfo(client):
+
+    sql_select_query = """select * from tasks where client = ?"""
+    conn = db.connect("tracker.db")
+    cur = conn.cursor()
+    cur.execute(sql_select_query, (client,))
+    records = cur.fetchall()
+    print("Total records for client:  ", len(records),"\n")
+    for row in records:
+        print("date = ", row[0])
+        print("warehouse  = ", row[1])
+        print("client  = ", row[2])
+        print("task  = ", row[3])
+        print("hours  = ", row[4],"\n")
+
+    cur.close()
+
+req_client = input("Enter client to view data: ",)
+print("\n")
+    
+getClientInfo(req_client)
